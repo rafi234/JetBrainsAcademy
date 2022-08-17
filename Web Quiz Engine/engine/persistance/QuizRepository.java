0de3af0c48b6cdaf96102f7368a12f227dbe7f72
rafi234 <73369159+rafi234@users.noreply.github.com>
2022-08-17 23:43:44 +0200

    Delete Web Quiz Engine directory

--- Web Quiz Engine/engine/persistance/QuizRepository.java
+++
@@ -1,8 +0,0 @@
-package engine.persistance;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends PagingAndSortingRepository<Quiz, Integer> {
-}
